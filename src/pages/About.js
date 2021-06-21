1785e60d8a49f0439b2950caf8043bc3ea6b86f9
ugurderdimet <61950938+ugurcemderdimet@users.noreply.github.com>
2021-06-21 11:49:23 +0300

    changed about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,16 +3,23 @@ import React from "react";
 export default function About() {
   return (
     <>
-    <section className="section about-section">
-      <h1 className="section-title">about us</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
-        repudiandae architecto qui adipisci in officiis, aperiam sequi atque
-        perferendis eos, autem maiores nisi saepe quisquam hic odio consectetur
-        nobis veritatis quasi explicabo obcaecati doloremque? Placeat ratione
-        hic aspernatur error blanditiis?
-      </p>
-    </section>
+      <section className="section about-section">
+        <h1 className="section-title">About Me</h1>
+        <p>
+          I graduated from Maçka Akif Tuncel Anatolian Technical High School
+          Information Technologies department with 2. highest ranked student
+          degree in 2015. I graduated from Beykent University Software
+          Engineering (EN / TR) department and IS'te YBL program, which is a
+          joint project of Turkiye Is Bankasi and Yenibirlider Association. I am
+          currently doing my master's degree in Yıldız Technical University
+          Computer Engineering Department. I am a software engineer who is
+          always open to learning, motivated, suitable for teamwork and strong
+          interpersonal communication. I am also a traveler and I am happy to
+          meet new people and get to know them and their lives. It is my
+          interest to follow countries' economies, governments and international
+          relations.
+        </p>
+      </section>
     </>
   );
 }
